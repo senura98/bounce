1d8148e4a1e5cdb895a34cb2760ff863e6263f76
Senura Weerasinghe <senuranisal1998@gmail.com>
2024-10-07 06:42:09 +0530

    bug fix

--- frontend/src/pages/MarsRoverDetail.jsx
+++ frontend/src/pages/MarsRoverDetail.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';  // Import useNavigate for navigation
-import Marschart from '../subcomponents/Marschart';
+import Marschart from '../components/subcomponents/Marschart';
 import MarsSolCards from '../cards/MarsSolCards';
 import MarsIcon from '../assets/marsIcon.svg';  // Import your SVG file
 
